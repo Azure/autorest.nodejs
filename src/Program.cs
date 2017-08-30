b8750e6a3ee9d7e7a23fc2e83f5c3ea6c409d397
Johannes Bader <olydis@users.noreply.github.com>
2017-08-30 10:50:24 -0700

    new common package (#3)

--- src/Program.cs
+++ src/Program.cs
@@ -31,7 +31,7 @@ namespace AutoRest.NodeJS
         public static int Main(string[] args )
         {
             if(args != null && args.Length > 0 && args[0] == "--server") {
-                var connection = new Connection(Console.Out, Console.OpenStandardInput());
+                var connection = new Connection(Console.OpenStandardOutput(), Console.OpenStandardInput());
                 connection.Dispatch<IEnumerable<string>>("GetPluginNames", async () => new []{ "nodejs" });
                 connection.Dispatch<string, string, bool>("Process", (plugin, sessionId) => new Program(connection, plugin, sessionId).Process());
                 connection.DispatchNotification("Shutdown", connection.Stop);
