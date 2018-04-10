f3882c523c3e67dd194fcefd36c17ba1241ca780
Johannes Bader <jogus@live.de>
2018-04-10 11:10:38 -0700

    added missing property

--- test/vanilla/AcceptanceTests/complexTypesTests.ts
+++ test/vanilla/AcceptanceTests/complexTypesTests.ts
@@ -424,7 +424,8 @@ describe('nodejs', function () {
             "fishtype": "goblin",
             "age": 1,
             "birthday": new Date("2015-08-08T00:00:00.000Z"),
-            "jawsize": 5
+            "jawsize": 5,
+            "color": "pinkish-gray"
           }
         ],
         "fishtype": "smart_salmon",
